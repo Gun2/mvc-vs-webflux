a0a3e9cf8891f42ca30c2af59dccb2872377d416
gun2 <0dayz@naver.com>
2024-07-30 23:58:07 +0900

    게시판의 생성 request dto 객체를 builder 패턴으로 생성할 수 있도록 수정

--- common/board-dto-model/src/main/java/com/github/gun2/anycommon/board/BoardDto.java
+++ common/board-dto-model/src/main/java/com/github/gun2/anycommon/board/BoardDto.java
@@ -19,6 +19,8 @@ public class BoardDto {
     @Getter
     @Setter
     @ToString
+    @Builder
+    @AllArgsConstructor
     @NoArgsConstructor
     public static class BoardRequest {
         @NotBlank
