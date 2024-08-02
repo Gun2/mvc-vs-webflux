88abab6d7d745b6b8c1e9a08fda341da6304b131
gun2 <0dayz@naver.com>
2024-08-03 00:19:19 +0900

    게시판 생성 API의 성능을 측정하기 위한 서비스 로직 추가

--- app/measurement-app/src/main/java/com/github/gun2/measurementapp/component/BoardApiIncreasingClientMeasurementServiceFactory.java
+++ app/measurement-app/src/main/java/com/github/gun2/measurementapp/component/BoardApiIncreasingClientMeasurementServiceFactory.java
@@ -1,6 +1,7 @@
 package com.github.gun2.measurementapp.component;
 
 import com.github.gun2.measurementapp.service.BoardApiService;
+import com.github.gun2.measurementapp.service.BoardCreateIncreasingClientMeasurementService;
 import com.github.gun2.measurementapp.service.BoardReadIncreasingClientMeasurementService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,9 +13,22 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class BoardReadIncreasingClientMeasurementServiceFactory {
+public class BoardApiIncreasingClientMeasurementServiceFactory {
     private final BoardApiService boardApiService;
-    public BoardReadIncreasingClientMeasurementService create(){
+
+    /**
+     * 조회 API 성능 측정 서비스 생성
+     * @return
+     */
+    public BoardReadIncreasingClientMeasurementService createReadApiMeasurementService(){
         return new BoardReadIncreasingClientMeasurementService(this.boardApiService);
+    }
+
+    /**
+     * 생성 API 성능 측정 서비스 생성
+     * @return
+     */
+    public BoardCreateIncreasingClientMeasurementService createCreateApiMeasurementService(){
+        return new BoardCreateIncreasingClientMeasurementService(this.boardApiService);
     }
 }
---
+++ app/measurement-app/src/main/java/com/github/gun2/measurementapp/service/BoardCreateIncreasingClientMeasurementService.java
@@ -0,0 +1,50 @@
+package com.github.gun2.measurementapp.service;
+
+import com.github.gun2.anycommon.board.BoardDto;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+/**
+ * 게시판 생성 API 성능 측정 서비스
+ */
+@Slf4j
+public class BoardCreateIncreasingClientMeasurementService extends TemplateIncreasingClientMeasurementService {
+    private final BoardApiService boardApiService;
+    private final BoardDto.BoardRequest sample = getSample();
+
+    public BoardCreateIncreasingClientMeasurementService(BoardApiService boardApiService) {
+        super(TemplateIncreasingClientMeasurementConfig.builder()
+                .phase(10)
+                .initClient(100)
+                .increasingClient(100)
+                .outputPath("board_create_output.json")
+                .durationMsPerPhase(5000)
+                .build());
+        this.boardApiService = boardApiService;
+    }
+
+    @Override
+    public Mono<BoardDto> request(){
+        return boardApiService.create(sample);
+    }
+
+
+    private void warmUp(){
+        for (int i = 0; i < 100; i++) {
+            request().block();
+        }
+    }
+
+
+    private BoardDto.BoardRequest getSample() {
+        return BoardDto.BoardRequest.builder()
+                .title("test title")
+                .content("test content")
+                .build();
+    }
+
+    @Override
+    public void ready() {
+        warmUp();
+    }
+}
