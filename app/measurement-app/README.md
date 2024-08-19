658fd05b9f5f477e28b0110560c902900b518ecd
gun2 <0dayz@naver.com>
2024-08-20 01:11:00 +0900

    사용하지 않는 설정 밎 로직 제거 > 측정 모듈이 properties를 통해 설정값을 부여받아 측정을 진행하는 방식에서 shell command 방식으로 변경되므로써 properties로 설정값을 사용하던 코드 제거

--- app/measurement-app/README.md
+++ app/measurement-app/README.md
@@ -1,17 +1,1 @@
 # 성능 측정을 위한 모듈
-
-## properties
-
-| key                                                 | 설명                         | 타입     | 기본값                      |
-|-----------------------------------------------------|----------------------------|--------|--------------------------|
-| app.target.url                                      | (읽기)측정 대상 URL              | string | ${API_TARGET_URL}        |
-| app.read.measurement.config.init-client             | (읽기)성능 측정 시 초기 요청 클라이언트 개수 | number | 100                      |
-| app.read.measurement.config.increasing-client       | (읽기)매 단계 마다 증가시킬 클라이언트 개수  | number | 100                      |
-| app.read.measurement.config.duration-ms-per-phase   | (읽기)단계가 유지되는 시간 (ms)       | number | 5000                     |
-| app.read.measurement.config.phase                   | (읽기)성능 측정 단계 수             | number | 10                       |
-| app.read.measurement.config.output-path             | (읽기)측정 결과 저장 경로            | string | board_read_output.json   |
-| app.create.measurement.config.init-client           | (쓰기)성능 측정 시 초기 요청 클라이언트 개수 | number | 100                      |
-| app.create.measurement.config.increasing-client     | (쓰기)매 단계 마다 증가시킬 클라이언트 개수  | number | 100                      |
-| app.create.measurement.config.duration-ms-per-phase | (쓰기)단계가 유지되는 시간 (ms)       | number | 5000                     |
-| app.create.measurement.config.phase                 | (쓰기)성능 측정 단계 수             | number | 10                       |
-| app.create.measurement.config.output-path           | (쓰기)측정 결과 저장 경로            | string | board_create_output.json |
--- app/measurement-app/src/main/java/com/github/gun2/measurementapp/component/BoardApiIncreasingClientMeasurementServiceFactory.java
+++ app/measurement-app/src/main/java/com/github/gun2/measurementapp/component/BoardApiIncreasingClientMeasurementServiceFactory.java
@@ -1,6 +1,5 @@
 package com.github.gun2.measurementapp.component;
 
-import com.github.gun2.measurementapp.properties.AppInfo;
 import com.github.gun2.measurementapp.service.BoardApiService;
 import com.github.gun2.measurementapp.service.BoardCreateIncreasingClientMeasurementService;
 import com.github.gun2.measurementapp.service.BoardReadIncreasingClientMeasurementService;
@@ -17,39 +16,21 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class BoardApiIncreasingClientMeasurementServiceFactory {
     private final BoardApiService boardApiService;
-    private final AppInfo appInfo;
 
     /**
      * 조회 API 성능 측정 서비스 생성
      * @return
      */
-    public BoardReadIncreasingClientMeasurementService createReadApiMeasurementService(){
-        return createReadApiMeasurementService(convert(appInfo.getReadMeasurementConfig()));
-    }
-
     public BoardReadIncreasingClientMeasurementService createReadApiMeasurementService(TemplateIncreasingClientMeasurementConfig config){
         return new BoardReadIncreasingClientMeasurementService(this.boardApiService, config);
     }
+
 
     /**
      * 생성 API 성능 측정 서비스 생성
      * @return
      */
-    public BoardCreateIncreasingClientMeasurementService createCreateApiMeasurementService(){
-        return createCreateApiMeasurementService(convert(appInfo.getCreateMeasurementConfig()));
-    }
-
     public BoardCreateIncreasingClientMeasurementService createCreateApiMeasurementService(TemplateIncreasingClientMeasurementConfig config){
         return new BoardCreateIncreasingClientMeasurementService(this.boardApiService, config);
-    }
-
-    public TemplateIncreasingClientMeasurementConfig convert(AppInfo.MeasurementConfig measurementConfig){
-        return TemplateIncreasingClientMeasurementConfig.builder()
-                .phase(measurementConfig.getPhase())
-                .initClient(measurementConfig.getInitClient())
-                .increasingClient(measurementConfig.getIncreasingClient())
-                .outputPath(measurementConfig.getOutputPath())
-                .durationMsPerPhase(measurementConfig.getDurationMsPerPhase())
-                .build();
     }
 }
